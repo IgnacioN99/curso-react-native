0ae73b06a6bb1486ee99782f900f7f90ddbbce8c
Ignacio Flores <inifl99@gmail.com>
2024-05-20 09:25:07 -0300

    initial commit

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+const AppPrueba = () => {
+  return (
+    <View>
+      <Text>Alooo</Text>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('AppPrueba', () => AppPrueba);
